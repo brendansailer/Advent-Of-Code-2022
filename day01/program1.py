aec0d3ba41d5c6e7d4ec7dcee68ddeea00d1c3a8
brendansailer <57545584+brendansailer@users.noreply.github.com>
2022-12-01 00:02:19 -0800

    Day01: Complete

--- day01/program1.py
+++ day01/program1.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 
+import sys
+
+largest = 0
+running_total = 0
+
+for line in sys.stdin:
+    if line == '\n':
+        largest = max(largest, running_total)
+        running_total = 0
+    else:
+        running_total += int(line.strip())
+
+print(largest)
--- day01/program2.py
+++ day01/program2.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python3
 
+# Save time and space
+import sys
+import heapq
+
+largest = []
+running_total = 0
+
+for line in sys.stdin:
+    if line == '\n':
+        heapq.heappush(largest, running_total)
+        running_total = 0
+
+        # Don't let the heap have more than 3 elements
+        if len(largest) >= 4:
+            heapq.heappop(largest)
+    else:
+        running_total += int(line.strip())
+
+print(sum(largest))
+
+# Brute force
+'''
+import sys
+
+largest = []
+running_total = 0
+
+for line in sys.stdin:
+    if line == '\n':
+        largest.append(running_total)
+        running_total = 0
+    else:
+        running_total += int(line.strip())
+
+print(sum(sorted(largest)[len(largest)-3:]))
+'''
