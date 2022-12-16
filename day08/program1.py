57c683c041d311b46b21e2757e447000635b0253
Brendan <brendans99@gmail.com>
2022-12-16 10:38:32 -0800

    Day08: Complete

--- day08/program1.py
+++ day08/program1.py
@@ -7,30 +7,30 @@ grid = [[int(tree) for tree in line.strip()] for line in sys.stdin]
 len_x, len_y = len(grid[0]), len(grid)
 result = [[True for _ in range(len_x)] for _ in range(len_y)] # True = Hidden
 
-for y in range(1, len_y):
+for y in range(1, len_y-1):
     max_height = grid[y][0]
-    for x in range(1, len_x):
+    for x in range(1, len_x-1):
         if grid[y][x] > max_height:
             result[y][x] = False
             max_height = grid[y][x]
 
-for y in range(1, len_y):
+for y in range(1, len_y-1):
     max_height = grid[y][-1]
-    for x in range(len_x-1, -1, -1):
+    for x in range(len_x-1, 0, -1):
         if grid[y][x] > max_height:
             result[y][x] = False
             max_height = grid[y][x]
 
-for x in range(1, len_x):
+for x in range(1, len_x-1):
     max_height = grid[0][x]
-    for y in range(1, len_y):
+    for y in range(1, len_y-1):
         if grid[y][x] > max_height:
             result[y][x] = False
             max_height = grid[y][x]
 
-for x in range(1, len_x):
+for x in range(1, len_x-1):
     max_height = grid[-1][x]
-    for y in range(len_y-1, -1, -1):
+    for y in range(len_y-1, 0, -1):
         if grid[y][x] > max_height:
             result[y][x] = False
             max_height = grid[y][x]
@@ -41,5 +41,5 @@ for row in result[1:-1]:
         if value == True:
             count += 1
 
-print(len_x, len_y, len_x*len_y)
-print(count)
+total_trees = len_x*len_y
+print(f'Result: {total_trees-count}')
